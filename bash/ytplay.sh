a782ae54314322c062b9527de5aecb9729beb127
octarin <octarin.npn@gmail.com>
2014-06-30 12:50:15 +0200

    Update ytplay.sh

--- bash/ytplay.sh
+++ bash/ytplay.sh
@@ -18,7 +18,7 @@ if [[ -z $1 ]]; then
 	EXAMPLES OF USE:
 
 	ytplay.sh \"asdf movie\"                   # Will recense the list of the asdf movies
-	vlc $\(ytplay.sh \"Atom Heart Mother\"\) 1 # Will play Atom Heart Mother with vlc
+	vlc $\(ytplay.sh \"Atom Heart Mother\" 1\)   # Will play Atom Heart Mother with vlc
 	"
 
 fi
