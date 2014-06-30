8aacd7eca37f584a7160feb28835a6fd49c6a3f8
octarin <octarin.npn@gmail.com>
2014-06-30 12:40:44 +0200

    Delete play.sh

--- bash/play.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z $2 ]]; then
-    nb=1
-else
-    nb=$2
-fi
-
-vlc $(curl -s www.youtube.com/results?search_query=$1|sed -rn "s/.*href=\"(\/watch\?v=[^\"]+)\".*/http:\/\/www.youtube.com\1/p"|head -n $nb)
