cc2385bce3a6b453fcb56e821d9c86f51e1ce32f
octarin <octarin.npn@gmail.com>
2014-08-04 11:04:18 +0200

    Removed shit from the code

--- python/bip.py
+++ python/bip.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python2
 
+# You shall couple it with aplay to make it work ;)
+
 import sys
 
 t = 0x2055418
-# t = 0x20554181354654656435416564616541
 
 while 1:
     c = ((t & 60) * (t >> 15) | t*(((t>>15)|(t>>8))&(60&(t>>12)))) & 0xff
-    # print(chr(c), end="")
     bug = sys.stdout.write(chr(c))
     t+=1
-    # t+=5
 
