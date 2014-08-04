8f88977a9f2df423ce612b7764670cafad9df4e1
octarin <octarin.npn@gmail.com>
2014-08-04 10:38:02 +0200

    Clarification of the code

--- python/gen.py
+++ python/gen.py
@@ -2,7 +2,6 @@
 
 import sys
 import os
-import subprocess
 import re
 
 #Some variables you can modify
@@ -11,45 +10,36 @@ NOMBRE_IMG_PAGE = 50
 TYPES_IMG = "\.(jpg)|(png)|(bmp)|(jpeg)|(gif)|(PNG)|(JPG)$"
 
 def makepage(numero, img_list):
-    fd = open("{0}{1}.html".format(NOM_PAGE_BASE, numero), 'w')
-    sys.stdout = fd
+    buf ='<html>\n\t<head>\n\t\t<meta charset="utf-8" />\n' \
+        '\t\t<title>Liste des images du dossier</title>\n' \
+        '\t\t<link rel="stylesheet" href="style.css" />\n' \       #You can add your own stylesheet
+        '\t</head>\n\t<body>\n' \
+        '\n\t\t<p>'
 
-    print('<html>\n\t<head>\n\t\t<meta charset="utf-8" />\n\t\t<meta name="generator" content="Python generator (c) Pierre Bonnet aka octarin" />')
-    print('\t\t<title>Liste des images du dossier</title>')
-    print('\t\t<link rel="stylesheet" href="style.css" />')
-    print('\t</head>\n\t<body>')
-
-    print("\n\t\t<p>")
+    nav = ''
     if numero > 0:
-        print('\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Précédent</a>'.format(NOM_PAGE_BASE, numero-1))
+        nav += '\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Précédent</a>\n'.format(NOM_PAGE_BASE, numero-1)
     if numero < int(len(img_list) / NOMBRE_IMG_PAGE):
-        print('\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Suivant</a>'.format(NOM_PAGE_BASE, numero+1))
+        nav += '\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Suivant</a>\n'.format(NOM_PAGE_BASE, numero+1)
 
-    print("\t\t</p>")
+    buf += nav + "\t\t</p>"
 
     for i in range(NOMBRE_IMG_PAGE*numero, NOMBRE_IMG_PAGE*(numero+1)-1):
         if i < len(img_list):
-            print('\t\t<a href="{0}"><img src="{0}" alt="{0}" height=200/></a>'.format(img_list[i]))
+            buf += '\t\t<a href="{0}"><img src="{0}" alt="{0}" height=200/></a>'.format(img_list[i])
 
-    print("\t\t<p>")
-    if numero > 0:
-        print('\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Précédent</a>'.format(NOM_PAGE_BASE, numero-1))
-    if numero < int(len(img_list) / NOMBRE_IMG_PAGE):
-        print('\t\t\t<a href="{0}{1}.html" alt="{0}{1}.html">Suivant</a>'.format(NOM_PAGE_BASE, numero+1))
+    buf += '\t\t<p>' + nav + '\t\t</p>' + '\n\t</body>\n</html>'
 
-    print("\t\t</p>")
+    with open("{0}{1}.html".format(NOM_PAGE_BASE, numero if numero else ''), 'w') as fd:
+        fd.write(buf)
 
-    print('\n\t</body>\n</html>')
-
-    sys.stdout = sys.__stdout__
-    fd.close()
 
 os.system('rm {0}*.html'.format(NOM_PAGE_BASE))
 
-img_list = sorted([i for i in os.listdir('.') if re.search(TYPES_IMG, i) != None])
+img_list = sorted([file_ for file_ in os.listdir('.') if re.search(TYPES_IMG, file_) != None])
 
-for i in range(0, int(len(img_list) / NOMBRE_IMG_PAGE) + 1):
-    makepage(i, img_list)
+for nb_page in range(0, int(len(img_list) / NOMBRE_IMG_PAGE) + 1):
+    makepage(nb_page, img_list)
 
 sys.exit(0)
 
