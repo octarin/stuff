77f3cee690087e742a711d5618c8135e432514b7
octarin <octarin.npn@gmail.com>
2014-08-14 10:29:03 +0200

    Make it loadable

--- python/bin2str.py
+++ python/bin2str.py
@@ -4,6 +4,7 @@ from sys import argv
 
 bin2str = lambda string: "".join([chr(int(string[i*8:i*8+8], 2)) for i in range(len(string)//8)])
 
-if len(argv) > 1:
-    print(bin2str(argv[1]))
+if __name__ == '__main__':
+    if len(argv) > 1:
+        print(bin2str(argv[1]))
 
